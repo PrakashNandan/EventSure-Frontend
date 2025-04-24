734df865f3c47db6412f3971ae95b1931b14ea50
Prakash <prakashnandan27@gmail.com>
2025-04-25 00:10:03 +0530

    changes made in socket

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,5 +1,6 @@
 
 // const baseURL = "http://localhost:8000";
-const baseURL ="https://eventsure-backend.vercel.app"
+// const baseURL ="https://eventsure-backend.vercel.app"
+const baseURL = "https://event-sure-backend-hfc8.vercel.app";
 
 export default baseURL;
